c4d500a363305d5140771b2cf819b80ac8bc4ed2
Phil Reif <pr@sambal.net>
2022-06-30 14:55:00 +0200

    Extend Startup code to incude CORS and routing

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
 using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRazorPages();
+builder.Services.AddCors();
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseCors(options =>
+{
+    options.AllowAnyMethod().AllowAnyHeader();
+    options.SetIsOriginAllowed((host) => true);
+    options.AllowCredentials();
+});
+
 app.UseDefaultFiles();
 app.UseStaticFiles();
-app.MapDefaultControllerRoute();
-app.MapRazorPages();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapRazorPages();                
+    endpoints.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
+    endpoints.MapFallbackToFile("index.html").AllowAnonymous();
+});
 
 app.Run();
